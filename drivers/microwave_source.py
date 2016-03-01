1c02461acb5a3cd1c3966bc04dd19be28d65c0e0
Andre Schneider <andre.schneider@student.kit.edu>
2016-03-01 18:56:18 +0100

    driver fix

--- drivers/microwave_source.py
+++ drivers/microwave_source.py
@@ -10,13 +10,13 @@ def set_frequency(frequency):
 def get_all():
 	get_frequency()
 	get_power()
-	while True:
-		if get_status():
-			break
-		sleep(.5) #wait until the mw source is ready
+	#while True:
+	#	if get_status():
+	#		break
+	#	sleep(.5) #wait until the mw source is ready
 		
 def get_status():
-	return False #*STB? does not work for this device, just a quick fix...
+	return True #*STB? does not work for this device, just a quick fix...
 	
 	if vinstrument.ask("*STB?")=="0"
 		return True
